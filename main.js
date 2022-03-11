10458cc3f8d45834ff69da3f61c383d02a332aca
Dennis <ssembatyadennis25@gmail.com>
2022-03-12 00:29:59 +0300

    Write a function that prints the arguments of the passed in parameter to the console

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+Objective
+In this challenge, we review some basic concepts that will get you started with this series. Check out the tutorial to learn more about JavaScript's lexical structure.
+*/ 
+
+function greeting(parameterVariable) {
+    //This line prints "Hello, World!" to the console:
+    console.log('Hello, World!');
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
+} 
