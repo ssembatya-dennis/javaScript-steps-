c7e35c6c5ecc4b3e861f4c74e0e8c02edc4d3d41
Dennis <ssembatyadennis25@gmail.com>
2022-03-24 06:35:26 +0300

    Write a program that creates a string that represents an 8×8 grid (#Chessboard)

--- main.js
+++ main.js
@@ -103,3 +103,30 @@ for (let count = 0; count < 100; count++) {
     console.log(currentNumber);
   }
 }
+for (let chessbordLine = 0; chessbordLine < 8; chessbordLine++) {
+  if (chessbordLine % 2 === 1) {
+    // chessbord lines #1
+    let chessbordLineCharacter = " ";
+    for (let count = 0; count < 8; count++) {
+      if (count % 2 === 1) {
+        chessbordLineCharacter = chessbordLineCharacter + " ";
+      } else if (count % 2 === 0) {
+        chessbordLineCharacter = chessbordLineCharacter + "#";
+      }
+    }
+    ("/n");
+    console.log(chessbordLineCharacter);
+  } else if (chessbordLine % 2 === 0) {
+    //chessbord lines #2
+    let chessbordLineCharacter = " ";
+    for (let count = 0; count < 8; count++) {
+      if (count % 2 === 1) {
+        chessbordLineCharacter = chessbordLineCharacter + "#";
+      } else if (count % 2 === 0) {
+        chessbordLineCharacter = chessbordLineCharacter + " ";
+      }
+    }
+    ("/n");
+    console.log(chessbordLineCharacter);
+  }
+}
